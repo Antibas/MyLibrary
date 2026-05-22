ef8bfdc36ddcaed6321a6c696d690e0034b94a8d
Antibas <antivasisp@gmail.com>
2026-05-22 18:05:24 +0300

    Many additions

---
+++ src/main/java/mylib/math/function/ISeries.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mylib.math.function;
+
+
+/**
+ *
+ * @author User
+ */
+public interface ISeries extends java.util.function.Function<Integer, Double>{
+    public default double sum(int a, int b){
+        if(a >= b) throw new IllegalArgumentException();
+        double S = 0d;
+        for(int n = a; n <= b; n++){
+            S += apply(n);
+        }
+        return S;
+    }
+    
+    public default double limit(int l){
+        return apply(l-1);
+    }
+    
+}
